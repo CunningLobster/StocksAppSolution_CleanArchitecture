ffd975b209c66e958f7c0a6ef6e256b3bfc3c78a
Yan Cherepansky <cherepansky.yan@yandex.ru>
2023-08-22 11:05:34 +0600

    Add migrations

---
+++ StocksApp.Infrastructure/Migrations/20230822050239_Initial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StocksApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BuyOrders",
+                columns: table => new
+                {
+                    BuyOrderID = table.Column<Guid>(type: "uuid", nullable: false),
+                    StockSymbol = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    StockName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    DateAndTimeOfOrder = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Quantity = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuyOrders", x => x.BuyOrderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SellOrders",
+                columns: table => new
+                {
+                    SellOrderID = table.Column<Guid>(type: "uuid", nullable: false),
+                    StockSymbol = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    StockName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    DateAndTimeOfOrder = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Quantity = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SellOrders", x => x.SellOrderID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BuyOrders");
+
+            migrationBuilder.DropTable(
+                name: "SellOrders");
+        }
+    }
+}
